6cf2186fd359094ffedecf4ec2ca9eafd01805c4
patloew <patloew@users.noreply.github.com>
2017-02-28 08:52:48 +0100

    Update dependencies.

--- app/src/main/java/com/tailoredapps/template/injection/modules/NetModule.kt
+++ app/src/main/java/com/tailoredapps/template/injection/modules/NetModule.kt
@@ -2,7 +2,6 @@ package com.tailoredapps.template.injection.modules
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import com.tailoredapps.template.BuildConfig
 import com.tailoredapps.template.data.remote.MyApi
 import com.tailoredapps.template.injection.scopes.PerApplication
@@ -12,6 +11,7 @@ import io.reactivex.schedulers.Schedulers
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 /* Copyright 2016 Patrick Löwenstein
